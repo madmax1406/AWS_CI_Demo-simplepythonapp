7c9e58854bf232481c1e05782fc3f410ced6e9c5
madmax1406 <pushkarsane98@gmail.com>
2024-07-03 00:08:22 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "Pulling Image from Docker"
+docker pull madmax1406/simple-python-flask-app
+
 
 # Run the Docker image as a container
-echo
+echo "Running Docker container"
+docker run -d -p 5000:5000 madmax1406/simple-python-flask-app
