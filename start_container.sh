0911361cb0990b2703777aa3b608f66e123373b7
madmax1406 <pushkarsane98@gmail.com>
2024-07-10 10:47:29 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -9,3 +9,4 @@ docker pull madmax1406/simple-python-flask-app
 # Run the Docker image as a container
 echo "Running Docker container"
 docker run -d -p 5000:5000 madmax1406/simple-python-flask-app
+
